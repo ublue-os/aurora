473f03804ba3bfe3dd39c78c3ae163cec9bb830d
Adam Fidel <adam@fidel.id>
2024-12-25 17:18:56 -0600

    fix: remove pkcs11-provider workaround
    Co-authored-by: Jorge O. Castro <jorge.castro@gmail.com>

--- build_files/base/18-workarounds.sh
+++ build_files/base/18-workarounds.sh
@@ -7,9 +7,12 @@ if [[ -f /usr/bin/ld.bfd ]]; then
     ln -sf /usr/bin/ld.bfd /etc/alternatives/ld && ln -sf /etc/alternatives/ld /usr/bin/ld
 fi
 
-
 ## Pins and Overrides
 ## Use this section to pin packages in order to avoid regressions
-if [ "$FEDORA_MAJOR_VERSION" -eq "41" ]; then
-    rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2024-dd2e9fb225    
-fi
+# Remember to leave a note with rationale/link to issue for each pin!
+#
+# Example:
+#if [ "$FEDORA_MAJOR_VERSION" -eq "41" ]; then
+#    Workaround pkcs11-provider regression, see issue #1943
+#    rpm-ostree override replace https://bodhi.fedoraproject.org/updates/FEDORA-2024-dd2e9fb225
+#fi
