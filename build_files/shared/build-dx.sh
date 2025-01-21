5d33aaf4eabdd703ef711d9ccd4462c570a30e33
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-01-21 17:45:54 +0200

    feat(ci): Enable Docker Building, GHA Log Grouping

--- build_files/shared/build-dx.sh
+++ build_files/shared/build-dx.sh
@@ -29,6 +29,9 @@ sysctl -p
 # Fetch Install
 /ctx/build_files/dx/04-override-install-dx.sh
 
+# Branding Changes
+echo "Variant=Developer Experience" >> /usr/share/kde-settings/kde-profile/default/xdg/kcm-about-distrorc
+
 # Systemd and Disable Repos
 /ctx/build_files/dx/09-cleanup-dx.sh
 
