71408d19454b0fae2f0d4f01d5feba15fa2269ba
dreamyukii <49896401+dreamyukii@users.noreply.github.com>
2025-02-08 22:52:45 +0700

    remove sddm file and add gpu-screen-recorder to install-gaming-flatpaks recipe

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -7,13 +7,13 @@ set -eoux pipefail
 # Add Staging repo
 dnf5 -y copr enable ublue-os/staging
 
-# Fonts
+# Add Ubuntu Fonts
 dnf5 -y copr enable atim/ubuntu-fonts
 
 # Add Switcheroo Repo
 dnf5 -y copr enable sentry/switcheroo-control_discrete
 
-# Add openrazer repo
+# Add OpenRazer repo
 curl -Lo /etc/yum.repos.d/hardware:razer.repo https://openrazer.github.io/hardware:razer.repo
 
 # Enable Nerd fonts repo
