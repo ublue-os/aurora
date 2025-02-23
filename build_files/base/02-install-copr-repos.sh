6a2da4d66275b1534426caedef014f19f23cb98c
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-02-24 00:29:36 +0200

    chore: use `ublue-os/packages` for ublue-related packages (#234)
    * chore: use `ublue-os/packages` for ublue-related packages and bluefin metapackage
    
    * remove the sentry switcheroo repo too
    
    ---------
    
    Co-authored-by: Tulip Blossom <tulilirockz@outlook.com>

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -7,8 +7,8 @@ set -eoux pipefail
 # Add Staging repo
 dnf5 -y copr enable ublue-os/staging
 
-# Add Switcheroo Repo
-#dnf5 -y copr enable sentry/switcheroo-control_discrete
+# Add Packages repo
+dnf5 -y copr enable ublue-os/packages
 
 # Add OpenRazer repo
 dnf5 -y config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
