e7e12583691409a492faae09995ddb6d192c9df0
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-02-23 20:08:37 +0200

    remove the sentry switcheroo repo too

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -10,9 +10,6 @@ dnf5 -y copr enable ublue-os/staging
 # Add Packages repo
 dnf5 -y copr enable ublue-os/packages
 
-# Add Switcheroo Repo
-#dnf5 -y copr enable sentry/switcheroo-control_discrete
-
 # Add OpenRazer repo
 dnf5 -y config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
 
