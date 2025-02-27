da815cc1ac24cc27f3d123bf01062d0babbda287
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-02-27 12:13:48 +0200

    disable terra repo during cleanup

--- build_files/dx/09-cleanup-dx.sh
+++ build_files/dx/09-cleanup-dx.sh
@@ -27,9 +27,7 @@ sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 # NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 # Diable terra repos
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/terra-mesa.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/terra-extras.repo
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/terra-nvidia.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/terra.repo
 
 for i in /etc/yum.repos.d/rpmfusion-*; do
     sed -i 's@enabled=1@enabled=0@g' "$i"
