c9ab9565bd581b517a043c8eff657b4261414b48
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-02-27 15:44:34 +0000

    chore: install fira code, monaspace and starship from terra packages (#242)
    * chore: install fira code nerd-font from package
    
    * chore: install monaspace font from package
    
    * chore: install starship from package
    
    ---------
    
    Co-authored-by: Juha Uotila <10782843+inffy@users.noreply.github.com>

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -43,9 +43,6 @@ dnf5 -y swap \
 
 
 # Starship Shell Prompt
-curl --retry 3 -Lo /tmp/starship.tar.gz "https://github.com/starship/starship/releases/latest/download/starship-x86_64-unknown-linux-gnu.tar.gz"
-tar -xzf /tmp/starship.tar.gz -C /tmp
-install -c -m 0755 /tmp/starship /usr/bin
 # shellcheck disable=SC2016
 echo 'eval "$(starship init bash)"' >> /etc/bashrc
 
