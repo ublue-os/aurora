515e75bb09b57ee51aaab49a6999f8308906043c
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-03-14 08:10:24 +0200

    chore: swap devpod package to copr

--- build_files/dx/04-override-install-dx.sh
+++ build_files/dx/04-override-install-dx.sh
@@ -4,6 +4,11 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
+# swap devpod to use ublue copr
+dnf5 -y swap \
+    --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+        devpod devpod
+
 curl --retry 3 -Lo /tmp/kind "https://github.com/kubernetes-sigs/kind/releases/latest/download/kind-$(uname)-amd64"
 chmod +x /tmp/kind
 mv /tmp/kind /usr/bin/kind
