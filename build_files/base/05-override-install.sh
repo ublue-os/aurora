fed7eb12fe48aafa1298387c4727d21c273d2ddc
Adam Fidel <adam@fidel.id>
2025-03-17 22:57:16 -0500

    fix: maybe this will work?

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -5,13 +5,12 @@ echo "::group:: ===$(basename "$0")==="
 set -eoux pipefail
 
 # Patched shell
-dnf5 -y swap \
-  --repo=terra \
-  kf6-kio kf6-kio
+dnf5 -y remove kf6-kio-core kf6-kio-core-libs kf6-kio-doc kf6-kio-file-widgets kf6-kio-gui kf6-kio-widgets
+dnf5 -y --repo=terra install kf6-kio
 
 # Make sure KDE Frameworks and our kf6-kio are on the same version
 kf6_version=$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
-kf6_kio_version=$(rpm -qi kf6-kio-core | awk '/^Version/ {print $3}')
+kf6_kio_version=$(rpm -qi kf6-kio.switcheroo | awk '/^Version/ {print $3}')
 
 if [[ "$kf6_version" != "$kf6_kio_version" ]]; then
   echo "Mismatched kf6-kio version $kf6_kio_version. Check Terra's kf6-kio for $kf6_version"
