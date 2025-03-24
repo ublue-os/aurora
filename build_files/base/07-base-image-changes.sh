c29bf71d4b5899efa09bb746425cb9ae2d3c8196
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-03-24 12:34:03 +0200

    prepend the discover arguments
    stealing this from bazzite, this seesm a little bit cleaner way to do it
    :p

--- build_files/base/07-base-image-changes.sh
+++ build_files/base/07-base-image-changes.sh
@@ -29,7 +29,7 @@ sed -i 's@Keywords=@Keywords=konsole;console;@g' /usr/share/applications/org.gno
 cp /usr/share/applications/org.gnome.Ptyxis.desktop /usr/share/kglobalaccel/org.gnome.Ptyxis.desktop
 
 # Plasma Discover
-sed -i 's|^Exec=plasma-discover %F$|Exec=plasma-discover --backends flatpak-backend %F|' /usr/share/applications/org.kde.discover.desktop
+sed -i 's/^Exec=plasma-discover/& --backends flatpak-backend/' /usr/share/applications/org.kde.discover.desktop
 
 rm -f /etc/profile.d/gnome-ssh-askpass.{csh,sh} # This shouldn't be pulled in
 
