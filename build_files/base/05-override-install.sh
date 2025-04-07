bea242df252cae30237e3f2fe26c33e657743469
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-04-07 12:01:16 +0300

    swap kf6-kio and switcheroo-control

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -6,21 +6,22 @@ set -eoux pipefail
 
 
 # Patched shell
-if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
   dnf5 -y swap \
       --repo=terra-extras \
           kf6-kio kf6-kio.switcheroo-$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
 
-  # Fix for ID in fwupd
-  dnf5 -y swap \
-      --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-          fwupd fwupd
-
-  # Switcheroo patch
+# Switcheroo patch
   dnf5 -y swap \
       --repo=terra-extras \
           switcheroo-control switcheroo-control
+
+# Fix for ID in fwupd - fwupd package needs to be updated in staging copr to 2.x
+if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
+  dnf5 -y swap \
+      --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+          fwupd fwupd
 fi
+
 
 # TODO: Fedora 41 specific -- re-evaluate with Fedora 42
 # negativo's libheif is broken somehow on older Intel machines
