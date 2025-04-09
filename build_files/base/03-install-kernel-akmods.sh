149473640f269f2e8f4d56df400dd6c8f58ef97e
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-04-09 13:51:09 +0300

    fix: temporary change for rpmfusion mirrors

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -46,8 +46,8 @@ dnf5 -y install "${AKMODS[@]}"
 
 # RPMFUSION Dependent AKMODS
 dnf5 -y install \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
+  https://mirrors.ocf.berkeley.edu/rpmfusion/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
+  https://mirrors.ocf.berkeley.edu/rpmfusion/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm
 
 dnf5 -y install \
     v4l2loopback /tmp/akmods/kmods/*v4l2loopback*.rpm
