060e3dc6223dc76a25f1d5d3af190a423c380fbf
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-04-13 16:25:14 +0300

    fix: fix my oopsie

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -10,17 +10,16 @@ if [[ "$(rpm -E %fedora)" -eq "41" ]]; then
   dnf5 -y swap \
       --repo="terra-extras" \
       kf6-kio kf6-kio.switcheroo-$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
-  dnf5 versionlock add kf6-kio.switcheroo
+
 # Patched switcheroo-control
   dnf5 -y swap \
       --repo="terra-extras" \
       switcheroo-control switcheroo-control
-  dnf5 versionlock add switcheroo-control
+
 elif [[ "$(rpm -E %fedora)" -eq "42" ]]; then
   dnf5 -y swap \
       --repo="terra-extras" \
       kf6-kio kf6-kio.switcheroo-$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
-  dnf5 versionlock add kf6-kio.switcheroo
 fi
 
 if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
