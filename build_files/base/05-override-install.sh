fdd771564d589f3415602b615dc9cbee7a716e0f
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-04-14 13:43:34 +0300

    remove the if statements

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -5,26 +5,17 @@ echo "::group:: ===$(basename "$0")==="
 set -eoux pipefail
 
 # Patched shell and switcheroo-control
-if [[ "$(rpm -E %fedora)" -eq "41" ]]; then
   dnf5 -y swap \
       --repo="terra*" \
-      kf6-kio kf6-kio.switcheroo-$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
+          kf6-kio kf6-kio.switcheroo-$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
   dnf5 -y swap \
       --repo="terra*" \
-      switcheroo-control switcheroo-control
-elif [[ "$(rpm -E %fedora)" -eq "42" ]]; then
-    dnf5 -y swap \
-        --repo="terra*" \
-      kf6-kio kf6-kio.switcheroo-$(rpm -qi kf6-kcoreaddons | awk '/^Version/ {print $3}')
-    dnf5 -y swap \
-        --repo="terra*" \
-        switcheroo-control switcheroo-control
-fi
+          switcheroo-control switcheroo-control
 
-    # Fix for ID in fwupd
-    dnf5 -y swap \
-        --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
-        fwupd fwupd
+# Fix for ID in fwupd
+  dnf5 -y swap \
+      --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
+          fwupd fwupd
 
 # TODO: Fedora 42 specific -- re-evaluate with Fedora 43
 # negativo's libheif is broken somehow on older Intel machines
