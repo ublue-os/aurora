5c6a1dd642cab5a7bdc322363eba8c1c0affc842
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-04-16 15:54:41 +0300

    feat: enable sunshine + add ujust script (#355)
    * add lizardbyte copr for sunshine
    
    * add sunshine package
    
    * globally disable sunshine service
    
    * add ujust configure script for sunshine
    
    * disable sunshine repo during cleanup

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -20,6 +20,9 @@ systemctl --global enable ublue-user-setup.service
 systemctl --global enable podman-auto-update.timer
 systemctl enable check-sb-key.service
 
+# disable sunshine service
+systemctl --global disable sunshine.service
+
 # Updater
 if systemctl cat -- uupd.timer &> /dev/null; then
     systemctl enable uupd.timer
@@ -49,6 +52,7 @@ dnf5 -y copr disable ublue-os/staging
 dnf5 -y copr disable ublue-os/packages
 dnf5 -y copr disable phracek/PyCharm
 dnf5 -y copr disable bieszczaders/kernel-cachyos-addons
+dnf5 -y copr disable lizardbyte/beta
 
 # NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
