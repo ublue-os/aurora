a78e09fd955e03c40744e0ab5c9657a6b70c7044
Adam Fidel <ledif@users.noreply.github.com>
2025-04-16 03:39:47 -0500

    chore: remove akmod workaround for beta (#349)
    Co-authored-by: Niklas <40402114+NiHaiden@users.noreply.github.com>
    Co-authored-by: Juha Uotila <10782843+inffy@users.noreply.github.com>

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -4,11 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-# Beta Updates Testing Repo...
-if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
-  dnf5 config-manager setopt updates-testing.enabled=1
-fi
-
 # Remove Existing Kernel
 for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
   rpm --erase $pkg --nodeps
@@ -36,18 +31,13 @@ dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel
 # Everyone
 # NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
 sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
-if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
-    dnf5 -y install /tmp/akmods/kmods/*xone*.rpm || true
-    dnf5 -y install /tmp/akmods/kmods/*xpadneo*.rpm || true
-    dnf5 -y install /tmp/akmods/kmods/*openrazer*.rpm || true
-    dnf5 -y install /tmp/akmods/kmods/*framework-laptop*.rpm || true
-else
-    dnf5 -y install \
-        /tmp/akmods/kmods/*xone*.rpm \
-        /tmp/akmods/kmods/*xpadneo*.rpm \
-        /tmp/akmods/kmods/*openrazer*.rpm \
-        /tmp/akmods/kmods/*framework-laptop*.rpm
-fi
+AKMODS=(
+    /tmp/akmods/kmods/*xone*.rpm
+    /tmp/akmods/kmods/*xpadneo*.rpm
+    /tmp/akmods/kmods/*framework-laptop*.rpm
+    /tmp/akmods/kmods/*openrazer*.rpm
+)
+dnf5 -y install "${AKMODS[@]}"
 
 # Install v4l2loopback from terra or gracefully fail
 dnf5 -y install --repo="terra" /tmp/akmods/kmods/*v4l2loopback*.rpm || true
