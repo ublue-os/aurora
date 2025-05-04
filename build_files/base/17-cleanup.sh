f8c9e656adce0a9b9b16cfa37dc3de1b96b4eaa7
Tulip Blossom <tulilirockz@proton.me>
2025-05-03 22:26:24 -0300

    feat: use ublue-setup-services for setup services
    This centralizes our setup service solution to the `ublue-setup-services` package, all these snippets came straight from Bluefin LTS.
    
    I believe this should work fine, but it needs some testing, just to make sure everything is okay, although I am quite confident that this is fine because we've been using this for months on LTS

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -11,15 +11,13 @@ set -eoux pipefail
 systemctl enable rpm-ostree-countme.service
 systemctl enable tailscaled.service
 systemctl enable dconf-update.service
-systemctl enable ublue-fix-hostname.service
-systemctl --global enable ublue-flatpak-manager.service
-systemctl enable ublue-system-setup.service
-systemctl enable ublue-guest-user.service
 systemctl enable brew-setup.service
 systemctl enable brew-upgrade.timer
 systemctl enable brew-update.timer
 systemctl enable aurora-groups.service
 systemctl enable usr-share-sddm-themes.mount
+systemctl enable ublue-fix-hostname.service
+systemctl enable ublue-system-setup.service
 systemctl --global enable ublue-user-setup.service
 systemctl --global enable podman-auto-update.timer
 systemctl enable check-sb-key.service
