0219a8da7343e3075d1f240f5d8f5c3e662cd58a
m2 <69128853+m2Giles@users.noreply.github.com>
2025-05-17 19:22:51 -0400

    reorder
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- iso_files/configure_iso_anaconda.sh
+++ iso_files/configure_iso_anaconda.sh
@@ -10,30 +10,6 @@ sbkey='https://github.com/ublue-os/akmods/raw/main/certs/public_key.der'
 
 # Configure Live Environment
 
-. /etc/os-release
-if [[ "$IMAGE_TAG" =~ gts|lts ]]; then
-    echo "Aurora ${IMAGE_TAG^^} release $VERSION_ID (${VERSION_CODENAME:='Big Star'})" >/etc/system-release
-else
-    echo "Aurora release $VERSION_ID ($VERSION_CODENAME)" >/etc/system-release
-fi
-
-sed 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=" "/' /usr/{,s}bin/liveinst
-
-# Setup dock
-tee /usr/share/glib-2.0/schemas/zz2-org.gnome.shell.gschema.override <<EOF
-[org.gnome.shell]
-welcome-dialog-last-shown-version='4294967295'
-favorite-apps = ['liveinst.desktop', 'documentation.desktop', 'discourse.desktop', 'org.mozilla.firefox.desktop', 'org.gnome.Nautilus.desktop']
-EOF
-
-# don't autostart gnome-software session service
-rm -f /etc/xdg/autostart/org.gnome.Software.desktop
-
-# disable the gnome-software shell search provider
-tee /usr/share/gnome-shell/search-providers/org.gnome.Software-search-provider.ini <<EOF
-DefaultDisabled=true
-EOF
-
 glib-compile-schemas /usr/share/glib-2.0/schemas
 
 systemctl disable rpm-ostree-countme.service
@@ -77,6 +53,15 @@ else
     fi
 fi
 dnf install -y "${SPECS[@]}"
+
+# Configure
+. /etc/os-release
+if [[ "$IMAGE_TAG" =~ gts|lts ]]; then
+    echo "Aurora ${IMAGE_TAG^^} release $VERSION_ID (${VERSION_CODENAME:='Big Bang'})" >/etc/system-release
+else
+    echo "Aurora release $VERSION_ID ($VERSION_CODENAME)" >/etc/system-release
+fi
+sed 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=" "/' /usr/{,s}bin/liveinst || true
 
 # Get Artwork
 git clone --depth=1 https://github.com/ublue-os/packages.git /root/packages
