ublue-os/bluefin

make our own profiles
Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

iso_files/configure_iso_anaconda.sh
@@ -44,44 +44,105 @@ systemctl --global disable ublue-user-setup.service
 
 # Configure Anaconda
 
-# Anaconda Profile Detection
-# TODO: Make our own profiles to not need to do this
-if [[ "$IMAGE_TAG" =~ lts ]]; then
-    echo 'VARIANT_ID=silverblue' >>/usr/lib/os-release
-else
-    sed -i 's/^VARIANT_ID=.*/VARIANT_ID=silverblue/' /usr/lib/os-release
-fi
-sed -i 's/^ID=.*/ID=fedora/' /usr/lib/os-release
-
 # Install Anaconda, Webui if >= F42
 SPECS=(
-    "libblockdev-btrfs"
     "libblockdev-lvm"
     "libblockdev-dm"
 )
 if [[ "$IMAGE_TAG" =~ lts ]]; then
     SPECS+=("anaconda-liveinst")
 else
-    SPECS+=("anaconda-live")
+    SPECS+=(
+        "anaconda-live"
+        "libblockdev-btrfs"
+    )
     if [[ "$(rpm -E %fedora)" -ge 42 ]]; then
         SPECS+=("anaconda-webui")
     fi
 fi
 dnf install -y "${SPECS[@]}"
 
+# Anaconda Profile Detection
+
+# Bluefin GTS/Stable
+tee /etc/anaconda/profile.d/bluefin.conf <<'EOF'
+# Anaconda configuration file for Bluefin GTS/Stable
+
+[Profile]
+# Define the profile.
+profile_id = bluefin
+
+[Profile Detection]
+# Match os-release values
+os_id = bluefin
+
+[Network]
+default_on_boot = FIRST_WIRED_WITH_LINK
+
+[Bootloader]
+efi_dir = fedora
+menu_auto_hide = True
+
+[Storage]
+default_scheme = BTRFS
+btrfs_compression = zstd:1
+default_partitioning =
+    /     (min 1 GiB, max 70 GiB)
+    /home (min 500 MiB, free 50 GiB)
+    /var  (btrfs)
+
+[User Interface]
+custom_styleshee = /usr/share/anaconda/pixmaps/silverblue/fedora-silverblue.css
+hidden_spokes =
+    NetworkSpoke
+    PasswordSpoke
+    UserSpoke
+
+[Localization]
+use_geolocation = False
+EOF
+
+# Bluefin LTS
+tee /etc/anaconda/profile.d/bluefin-lts.conf <<'EOF'
+# Anaconda configuration file for Bluefin LTS
+
+[Profile]
+# Define the profile.
+profile_id = bluefin-lts
+base_profile = bluefin
+
+[Profile Detection]
+# Match os-release values
+os_id = bluefin
+variant_id = bluefin-lts
+
+# TODO: Figure out a better default partitioning scheme
+[Storage]
+file_system_type = xfs
+default_scheme = LVM
+default_partitioning =
+    /     (min 1 GiB, max 50 GiB)
+    /home (min 500 MiB, free 50 GiB)
+    /var  (min 500 MiB, free 50 GiB)
+EOF
+
+if [[ "${IMAGE_TAG}" =~ lts ]]; then
+    sed -i 's/^ID=.*/ID=bluefin/' /usr/lib/os-release
+    echo "VARIANT_ID=bluefin-lts" >>/usr/lib/os-release
+fi
+
 # Configure
 . /etc/os-release
 if [[ "$IMAGE_TAG" =~ gts|lts ]]; then
-    echo "Bluefin ${IMAGE_TAG^^} release $VERSION_ID (${VERSION_CODENAME:='Big Bird'})" >/etc/system-release
+    echo "Bluefin ${IMAGE_TAG^^} release $VERSION_ID (${VERSION_CODENAME:=Big Bird})" >/etc/system-release
 else
     echo "Bluefin release $VERSION_ID ($VERSION_CODENAME)" >/etc/system-release
 fi
-sed 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=" "/' /usr/{,s}bin/liveinst || true
+sed -i 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=""/' /usr/{,s}bin/liveinst || true
 
 # Get Artwork
 git clone --depth=1 https://github.com/ublue-os/packages.git /root/packages
 mkdir -p /usr/share/anaconda/pixmaps/silverblue
-cp -r /root/packages/bluefin/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/
 cp -r /root/packages/bluefin/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/silverblue/
 rm -rf /root/packages
 

ublue-os/aurora

iso_files/configure_iso_anaconda.sh
@@ -29,30 +29,63 @@ systemctl --global disable ublue-user-setup.service
 
 # Configure Anaconda
 
-# Anaconda Profile Detection
-# TODO: Make our own profiles to not need to do this
-if [[ "$IMAGE_TAG" =~ lts ]]; then
-    echo 'VARIANT_ID=kinoite' >>/usr/lib/os-release
-else
-    sed -i 's/^VARIANT_ID=.*/VARIANT_ID=kinoite/' /usr/lib/os-release
-fi
-sed -i 's/^ID=.*/ID=fedora/' /usr/lib/os-release
-
 # Install Anaconda, Webui if >= F42
 SPECS=(
-    "libblockdev-btrfs"
     "libblockdev-lvm"
     "libblockdev-dm"
 )
 if [[ "$IMAGE_TAG" =~ lts ]]; then
     SPECS+=("anaconda-liveinst")
 else
-    SPECS+=("anaconda-live")
+    SPECS+=(
+        "anaconda-live"
+        "libblockdev-btrfs"
+    )
     if [[ "$(rpm -E %fedora)" -ge 42 ]]; then
         SPECS+=("anaconda-webui")
     fi
 fi
 dnf install -y "${SPECS[@]}"
+
+# Anaconda Profile Detection
+
+# Aurora Stable
+tee /etc/anaconda/profile.d/aurora.conf <<'EOF'
+# Anaconda configuration file for Aurora Stable
+
+[Profile]
+# Define the profile.
+profile_id = aurora
+
+[Profile Detection]
+# Match os-release values
+os_id = aurora
+
+[Network]
+default_on_boot = FIRST_WIRED_WITH_LINK
+
+[Bootloader]
+efi_dir = fedora
+menu_auto_hide = True
+
+[Storage]
+default_scheme = BTRFS
+btrfs_compression = zstd:1
+default_partitioning =
+    /     (min 1 GiB, max 70 GiB)
+    /home (min 500 MiB, free 50 GiB)
+    /var  (btrfs)
+
+[User Interface]
+custom_styleshee = /usr/share/anaconda/pixmaps/fedora.css
+hidden_spokes =
+    NetworkSpoke
+    PasswordSpoke
+    UserSpoke
+
+[Localization]
+use_geolocation = False
+EOF
 
 # Configure
 . /etc/os-release
@@ -61,12 +94,11 @@ if [[ "$IMAGE_TAG" =~ gts|lts ]]; then
 else
     echo "Aurora release $VERSION_ID ($VERSION_CODENAME)" >/etc/system-release
 fi
-sed 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=" "/' /usr/{,s}bin/liveinst || true
+sed -i 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=""/' /usr/{,s}bin/liveinst || true
 
 # Get Artwork
 git clone --depth=1 https://github.com/ublue-os/packages.git /root/packages
 mkdir -p /usr/share/anaconda/pixmaps/
-cp -r /root/packages/aurora/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/
 cp -r /root/packages/aurora/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/
 rm -rf /root/packages
 