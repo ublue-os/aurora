97d1cbd7b48e40bab5e058ef5bbbf772b5d7d12d
renner <renner0@posteo.de>
2025-06-22 20:24:53 +0200

    fix: versionlock kernel-uki-virt

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -24,7 +24,7 @@ dnf5 -y install \
   /tmp/kernel-rpms/kernel-uki-virt-*.rpm
 
 
-dnf5 versionlock add kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+dnf5 versionlock add kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
 
 # Everyone
 # NOTE: we won't use dnf5 copr plugin for ublue-os/akmods until our upstream provides the COPR standard naming
