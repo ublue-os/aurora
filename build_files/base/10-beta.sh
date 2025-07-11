3eb119ca0853b4e48bf92200ec3d807118820c68
renner <80410025+renner0e@users.noreply.github.com>
2025-07-11 17:01:04 +0200

    feat(beta): replace discover with bazaar on panel and kickoff (#683)

--- build_files/base/10-beta.sh
+++ build_files/base/10-beta.sh
@@ -20,4 +20,9 @@ if jq -e '.["image-flavor"] | test("nvidia")' /usr/share/ublue-os/image-info.jso
   sed -i 's|^Exec=bazaar window --auto-service$|Exec=env GSK_RENDERER=opengl bazaar window --auto-service|' /usr/share/applications/io.github.kolunmi.Bazaar.desktop
 fi
 
+# Replace discover on Panel and Kickoff with bazaar
+sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
+sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.gnome.Ptyxis.desktop,io.github.kolunmi.Bazaar.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
+
+
 echo "::endgroup::"
