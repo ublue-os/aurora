e533a5f06ca95fa093bf49967dedf58c108ad86f
Adam Fidel <adam@fidel.id>
2025-07-11 06:23:16 -0500

    fix: only enable Bazaar KRunner plugin on beta

--- build_files/base/10-beta.sh
+++ build_files/base/10-beta.sh
@@ -9,6 +9,12 @@ fi
 
 sudo dnf5 install -y bazaar krunner-bazaar
 
+# For new users, enable Bazaar in KRunner + disable Discover results
+cat >> /usr/share/kde-settings/kde-profile/default/xdg/krunnerrc << 'EOF'
+krunner_appstreamEnabled=false
+bazaarrunnerEnabled=true
+EOF
+
 if jq -e '.["image-flavor"] | test("nvidia")' /usr/share/ublue-os/image-info.json >/dev/null; then
   sed -i 's|^Exec=bazaar window --auto-service$|Exec=env GSK_RENDERER=opengl bazaar window --auto-service|' /usr/share/applications/io.github.kolunmi.Bazaar.desktop
 fi
