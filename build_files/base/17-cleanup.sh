7b4e0265aee65689903e118f5c157a7449864618
inffy <10782843+inffy@users.noreply.github.com>
2025-08-18 08:22:14 +0300

    remove the rpmfusion

--- build_files/base/17-cleanup.sh
+++ build_files/base/17-cleanup.sh
@@ -62,9 +62,6 @@ dnf5 -y copr disable lizardbyte/beta
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/hardware:razer.repo
-for i in /etc/yum.repos.d/rpmfusion-*; do
-    sed -i 's@enabled=1@enabled=0@g' "$i"
-done
 
 if [ -f /etc/yum.repos.d/fedora-coreos-pool.repo ]; then
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-coreos-pool.repo
