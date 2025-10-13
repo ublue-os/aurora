a3990b120359f6832197ae4ad4e7fdf325e40e28
renner <renner0@posteo.de>
2025-10-13 11:51:59 +0200

    feat: remove discover

--- build_files/base/11-bazaar.sh
+++ build_files/base/11-bazaar.sh
@@ -7,23 +7,6 @@ set -eoux pipefail
 ### Bazaar
 echo "Installing Bazaar workarounds"
 
-# Hide Discover entries by renaming them (allows for easy re-enabling)
-discover_apps=(
-  "org.kde.discover.desktop"
-  "org.kde.discover.flatpak.desktop"
-  "org.kde.discover.notifier.desktop"
-  "org.kde.discover.urlhandler.desktop"
-)
-
-for app in "${discover_apps[@]}"; do
-  if [ -f "/usr/share/applications/${app}" ]; then
-    mv "/usr/share/applications/${app}" "/usr/share/applications/${app}.disabled"
-  fi
-done
-
-# These notifications are useless and confusing
-rm /etc/xdg/autostart/org.kde.discover.notifier.desktop
-
 # Replace discover on Panel and Kickoff with bazaar
 sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
 sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,systemsettings.desktop,org.kde.dolphin.desktop,org.kde.kate.desktop,org.gnome.Ptyxis.desktop,dev.getaurora.aurora-docs.desktop,io.github.kolunmi.Bazaar.desktop<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
