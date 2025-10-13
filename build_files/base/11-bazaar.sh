7948639f0159b782b8faf7774eea263495518f0c
renner <renner0@posteo.de>
2025-10-13 08:24:18 +0200

    feat: add stuff so flatpak bazaar can coexist with rpm

--- build_files/base/11-bazaar.sh
+++ build_files/base/11-bazaar.sh
@@ -30,3 +30,7 @@ sed -i '/<entry name="favorites" type="StringList">/,/<\/entry>/ s/<default>[^<]
 
 # Use Bazaar for Flatpak refs
 echo "application/vnd.flatpak.ref=io.github.kolunmi.Bazaar.desktop" >> /usr/share/applications/mimeapps.list
+
+# TODO: remove me when we fully switch to flatpak bazaar and are out of the transitionary period where we have both rpm and flatpak
+cp -r /usr/share/ublue-os/bazaar /etc
+sed -i 's|/usr/share/ublue-os/|/run/host/etc/|g' /etc/bazaar/config.yaml
