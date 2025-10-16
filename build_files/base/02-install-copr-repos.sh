bd6393ea888841168e3080f600bf138b6e1afe65
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-10-16 16:22:38 +0300

    update packages and repo files

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -11,7 +11,10 @@ dnf5 -y copr enable ublue-os/staging
 dnf5 -y copr enable ublue-os/packages
 
 # Add OpenRazer repo
+# not available for f43 yet
+if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
 dnf5 -y config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
+fi
 
 # Enable repo for scx-scheds
 dnf5 -y copr enable bieszczaders/kernel-cachyos-addons
