3926dcd60f2f5f0924ec0b4d15ed24f0c0878431
inffy <10782843+inffy@users.noreply.github.com>
2025-10-25 20:05:06 +0300

    Update build_files/base/05-override-install.sh
    Co-authored-by: Adam Fidel <ledif@users.noreply.github.com>

--- build_files/base/05-override-install.sh
+++ build_files/base/05-override-install.sh
@@ -20,7 +20,7 @@ echo 'eval "$(starship init bash)"' >> /etc/bashrc
 ghcurl "https://github.com/ryanoasis/nerd-fonts/releases/latest/download/NerdFontsSymbolsOnly.zip" --retry 3 -o /tmp/nerdfontsymbols.zip
 unzip /tmp/nerdfontsymbols.zip -d /tmp
 mkdir -p /usr/share/fonts/nerd-fonts/NerdFontsSymbolsOnly/
-mv /tmp/*.ttf /usr/share/fonts/nerd-fonts/NerdFontsSymbolsOnly/
+mv /tmp/SymbolsNerdFont*.ttf /usr/share/fonts/nerd-fonts/NerdFontsSymbolsOnly/
 
 # Bash Prexec
 curl --retry 3 -Lo /usr/share/bash-prexec https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec.sh
