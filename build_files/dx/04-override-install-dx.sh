83533f0f37d7f2f991cc711aeb9b96419265e354
renner <renner0@posteo.de>
2025-10-25 12:54:45 +0200

    chore: remove kind from image
    this is available in brew

--- build_files/dx/04-override-install-dx.sh
+++ build_files/dx/04-override-install-dx.sh
@@ -4,10 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-ghcurl "https://github.com/kubernetes-sigs/kind/releases/latest/download/kind-$(uname)-amd64" --retry 3 -o /tmp/kind
-chmod +x /tmp/kind
-mv /tmp/kind /usr/bin/kind
-
 # ls-iommu helper tool for listing devices in iommu groups (PCI Passthrough)
 DOWNLOAD_URL=$(ghcurl "https://api.github.com/repos/HikariKnight/ls-iommu/releases/latest" | jq -r '.assets[] | select(.name| test(".*x86_64.tar.gz$")).browser_download_url')
 ghcurl "$DOWNLOAD_URL" --retry 3 -o /tmp/ls-iommu.tar.gz
