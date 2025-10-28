86cbf2cdb901b1ff911c1aa6d8188506a8d418f4
renner <80410025+renner0e@users.noreply.github.com>
2025-10-28 08:25:22 +0100

    feat: remove rpm bazaar (#1159)

--- build_files/base/11-bazaar.sh
+++ build_files/base/11-bazaar.sh
@@ -27,12 +27,5 @@ rm /etc/xdg/autostart/org.kde.discover.notifier.desktop
 # Replace discover on Panel and Kickoff with bazaar
 sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
 
-# Symlink Discover to Bazaar
-ln -s /usr/share/applications/io.github.kolunmi.Bazaar.desktop /usr/share/applications/org.kde.discover.desktop
-
 # Use Bazaar for Flatpak refs
 echo "application/vnd.flatpak.ref=io.github.kolunmi.Bazaar.desktop" >> /usr/share/applications/mimeapps.list
-
-# TODO: remove me when we fully switch to flatpak bazaar and are out of the transitionary period where we have both rpm and flatpak
-cp -r /usr/share/ublue-os/bazaar /etc
-sed -i 's|/usr/share/ublue-os/|/run/host/etc/|g' /etc/bazaar/config.yaml
