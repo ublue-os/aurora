378638548d67d489b22222660d4f8f2996c9cb88
renner <renner0@posteo.de>
2025-11-02 07:14:08 +0100

    fix: stop lying about default wallpaper extension

--- build_files/base/01-branding.sh
+++ build_files/base/01-branding.sh
@@ -4,13 +4,11 @@ echo "::group:: ===$(basename "$0")==="
 
 # Branding for Images
 
-ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-7/contents/images/3840x2160.jxl /usr/share/backgrounds/default.png
-ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-7/contents/images/3840x2160.jxl /usr/share/backgrounds/default-dark.png
+# Set default wallpaper
+# sddm theme.conf uses .jxl for the background!
+# default-dark currently point to the same file
+ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-7/contents/images/3840x2160.jxl /usr/share/backgrounds/default.jxl
+ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-7/contents/images/3840x2160.jxl /usr/share/backgrounds/default-dark.jxl
 ln -sf /usr/share/backgrounds/aurora/aurora.xml /usr/share/backgrounds/default.xml
-
-# /usr/share/sddm/themes/01-breeze-fedora/theme.conf uses default.jxl for the background
-# We are lying about the extension
-ln -sf /usr/share/backgrounds/default.png /usr/share/backgrounds/default.jxl
-ln -sf /usr/share/backgrounds/default-dark.png /usr/share/backgrounds/default-dark.jxl
 
 echo "::endgroup::"
