edff7cdf5e880f54f7ef73e0b5049cb9d1cbfd73
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-11-02 13:28:24 +0200

    maybe little cleaner

--- build_files/base/18-workarounds.sh
+++ build_files/base/18-workarounds.sh
@@ -6,7 +6,8 @@ set -eoux pipefail
 
 # Wait for driver loading to be complete before starting SDDM so that graphics work
 # See https://github.com/sddm/sddm/issues/1917
-sed -i '/^StartLimitBurst=2$/a # Wait for driver loading to be complete so that graphics work\n# (https://github.com/sddm/sddm/issues/1917)\nWants=systemd-udev-settle.service\nAfter=systemd-udev-settle.service' /usr/lib/systemd/system/sddm.service
+sed -i '/^After=/ s/$/ systemd-udev-settle.service/' /usr/lib/systemd/system/sddm.service
+sed -i '/^StartLimitBurst=2$/a # Wait for driver loading to be complete so that graphics work\n# (https://github.com/sddm/sddm/issues/1917)\nWants=systemd-udev-settle.service' /usr/lib/systemd/system/sddm.service
 
 # Current aurora systems have the bling.sh and bling.fish in their default locations
 mkdir -p /usr/share/ublue-os/aurora-cli
