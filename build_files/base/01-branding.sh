ff2f9278f38c67bb2702e8437552d82513b2e566
renner <renner0@posteo.de>
2025-11-03 06:42:01 +0100

    chore: remove fedora plasma theme

--- build_files/base/01-branding.sh
+++ build_files/base/01-branding.sh
@@ -11,4 +11,8 @@ ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-7/contents/images/3840x216
 ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-7/contents/images/3840x2160.jxl /usr/share/backgrounds/default-dark.jxl
 ln -sf /usr/share/backgrounds/aurora/aurora.xml /usr/share/backgrounds/default.xml
 
+# we use our own theme
+# we can't remove plasma-lookandfeel-fedora package because it is a dependency for plasma-desktop
+rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedora.desktop/
+
 echo "::endgroup::"
