76d52585cb80dae76ae34b811a87ffe236134b31
Adam Fidel <ledif@users.noreply.github.com>
2025-11-04 11:23:04 -0600

    chore(ci): remove/mask fedora flatpaks service directly (#1196)
    Co-authored-by: Jordan Petridis <16531710+alatiera@users.noreply.github.com>
    Co-authored-by: Tulip Blossom <tulilirockz@outlook.com>
    Co-authored-by: inffy <10782843+inffy@users.noreply.github.com>
    Co-authored-by: renner <renner0@posteo.de>

--- build_files/shared/clean-stage.sh
+++ build_files/shared/clean-stage.sh
@@ -4,7 +4,9 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-dnf clean all
+# This comes last because we can't *ever* afford to ship fedora flatpaks on the image
+systemctl mask flatpak-add-fedora-repos.service
+rm -f /usr/lib/systemd/system/flatpak-add-fedora-repos.service
 
 rm -rf /.gitkeep
 
