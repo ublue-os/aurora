89d037f5660908e3339757a1eed99e4edb852e5d
renner <renner0@posteo.de>
2025-11-04 20:58:27 +0100

    feat: change icon of installer to scope
    also in a non-insane way

--- iso_files/configure_iso_anaconda-webui.sh
+++ iso_files/configure_iso_anaconda-webui.sh
@@ -94,12 +94,14 @@ sed -i '2s/$/;liveinst.desktop/' /usr/share/kde-settings/kde-profile/default/xdg
 echo "Aurora release $VERSION_ID ($VERSION_CODENAME)" >/etc/system-release
 
 sed -i 's/ANACONDA_PRODUCTVERSION=.*/ANACONDA_PRODUCTVERSION=""/' /usr/{,s}bin/liveinst || true
-sed -i 's|^Icon=.*|Icon=/usr/share/anaconda/pixmaps/fedora-logo-icon.png|' /usr/share/applications/liveinst.desktop || true
+
+desktop-file-edit --set-key=Icon --set-value=/usr/share/pixmaps/scope_installer.png /usr/share/applications/liveinst.desktop
 
 # Get Artwork
 git clone --depth=1 https://github.com/ublue-os/packages.git /root/packages
 mkdir -p /usr/share/anaconda/pixmaps/
 cp -r /root/packages/aurora/fedora-logos/src/anaconda/* /usr/share/anaconda/pixmaps/
+cp /root/packages/aurora/fedora-logos/src/misc/scope_installer.png /usr/share/pixmaps/
 rm -rf /root/packages
 
 # Interactive Kickstart
