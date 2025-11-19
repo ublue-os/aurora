ccc249d2b4dd56e31321bc6811a361ed1f500d0d
Juha Uotila <10782843+inffy@users.noreply.github.com>
2025-11-19 10:53:16 +0200

    chore: remove testing repo for beta

--- build_files/base/03-install-kernel-akmods.sh
+++ build_files/base/03-install-kernel-akmods.sh
@@ -4,12 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-# Beta Updates Testing Repo...
-if [[ "${UBLUE_IMAGE_TAG}" == "beta" ]]; then
-    dnf5 config-manager setopt updates-testing.enabled=1
-fi
-# Here we are installing kernels/kernel modules/nvidia
-
 # Remove Existing Kernel
 for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra; do
     rpm --erase $pkg --nodeps
