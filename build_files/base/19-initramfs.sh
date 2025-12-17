77f9c68928d1a02059b1775cc6957b5c3e617493
renner <renner0@posteo.de>
2025-12-17 18:02:16 +0100

    chore: move dracut config to command

--- build_files/base/19-initramfs.sh
+++ build_files/base/19-initramfs.sh
@@ -8,7 +8,7 @@ KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
 
 # Ensure Initramfs is generated
 export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add ostree -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add "ostree fido2 tpm2-tss pkcs11 pcsc" -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
 
 echo "::endgroup::"
