78cb9b1c763f9efc81faea627e62376d70a56f70
renner <renner0@posteo.de>
2026-01-04 15:12:29 +0100

    feat: set default wallpaper in common
    sister PR: https://github.com/get-aurora-dev/common/pull/88

--- build_files/base/05-branding.sh
+++ build_files/base/05-branding.sh
@@ -4,14 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 # Branding for Images
 
-# Set default wallpaper
-# sddm theme.conf uses .jxl for the background!
-# default-dark currently point to the same file
-# Make sure to update the tests!
-ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-9/contents/images/3840x2160.jxl /usr/share/backgrounds/default.jxl
-ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-9/contents/images/3840x2160.jxl /usr/share/backgrounds/default-dark.jxl
-ln -sf /usr/share/backgrounds/aurora/aurora.xml /usr/share/backgrounds/default.xml
-
 # sets default/pinned applications on the taskmanager applet on the panel, there is no nice way to do this
 # https://bugs.kde.org/show_bug.cgi?id=511560
 sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager<\/default>/' /usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml
