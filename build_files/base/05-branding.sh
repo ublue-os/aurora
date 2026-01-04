a5fac8ca6cc7a9642bf0676ad880f2bdd065890b
renner <80410025+renner0e@users.noreply.github.com>
2026-01-04 13:52:06 +0100

    feat: set coneheads wallpaper as default (#1584)

--- build_files/base/05-branding.sh
+++ build_files/base/05-branding.sh
@@ -8,8 +8,8 @@ echo "::group:: ===$(basename "$0")==="
 # sddm theme.conf uses .jxl for the background!
 # default-dark currently point to the same file
 # Make sure to update the tests!
-ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-8/contents/images/3840x2160.jxl /usr/share/backgrounds/default.jxl
-ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-8/contents/images/3840x2160.jxl /usr/share/backgrounds/default-dark.jxl
+ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-9/contents/images/3840x2160.jxl /usr/share/backgrounds/default.jxl
+ln -sf /usr/share/backgrounds/aurora/aurora-wallpaper-9/contents/images/3840x2160.jxl /usr/share/backgrounds/default-dark.jxl
 ln -sf /usr/share/backgrounds/aurora/aurora.xml /usr/share/backgrounds/default.xml
 
 # sets default/pinned applications on the taskmanager applet on the panel, there is no nice way to do this
