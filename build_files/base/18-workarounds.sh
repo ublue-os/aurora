f4bb9415068007436ebfda198a91936b30492b03
Adam Fidel <ledif@users.noreply.github.com>
2026-01-07 16:11:10 -0600

    chore(bling): remove unused aurora-cli directory (#1595)
    Co-authored-by: inffy <10782843+inffy@users.noreply.github.com>

--- build_files/base/18-workarounds.sh
+++ build_files/base/18-workarounds.sh
@@ -4,10 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-# Current aurora systems have the bling.sh and bling.fish in their default locations
-mkdir -p /usr/share/ublue-os/aurora-cli
-cp /usr/share/ublue-os/bling/* /usr/share/ublue-os/aurora-cli
-
 # Force Ptyxis version opened via dbus (e.g., keyboard shortcut) to use the proper shim
 # https://github.com/ublue-os/bazzite/pull/3620
 sed -i 's@Exec=/usr/bin/ptyxis@Exec=/usr/bin/kde-ptyxis@g' /usr/share/dbus-1/services/org.gnome.Ptyxis.service
