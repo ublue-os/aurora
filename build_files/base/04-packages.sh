51dabb8f80675c378fd9def079badf6ebd6cb881
inffy <10782843+inffy@users.noreply.github.com>
2026-01-26 08:16:11 +0000

    enable staging repo for fwupd swap

--- build_files/base/04-packages.sh
+++ build_files/base/04-packages.sh
@@ -260,6 +260,8 @@ rm -rf /usr/share/plasma/look-and-feel/org.fedoraproject.fedora.desktop/
 
 # https://github.com/ublue-os/bazzite/issues/1400
 # TODO: test if we still need this when upgrading firmware with fwupd
+dnf -y copr enable ublue-os/staging
+dnf -y copr disable ublue-os/staging
 dnf5 -y swap \
   --repo=copr:copr.fedorainfracloud.org:ublue-os:staging \
   fwupd fwupd
