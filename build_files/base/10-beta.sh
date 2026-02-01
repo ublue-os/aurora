fbb4b8d8a11d4d0860a1499ec96b2077cbbe8b0e
renner <80410025+renner0e@users.noreply.github.com>
2026-02-01 15:08:13 +0100

    fix: set RELEASE_TYPE as image tag for beta only (#1675)
    The standard value derived from fedora here is set to stable, this only
    makes sense to deviate from on our beta images as the latest image tag
    as RELEASE_TYPE would be awkward.

--- build_files/base/10-beta.sh
+++ build_files/base/10-beta.sh
@@ -4,12 +4,4 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-
-if ! jq -e '.["image-tag"] | test("beta|latest")' /usr/share/ublue-os/image-info.json >/dev/null; then
-    echo "We only run this on latest and beta"
-    exit 0
-fi
-
-
-
 echo "::endgroup::"
