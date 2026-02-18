9299f26859c35853f63169094d3989f5f403aff2
dreamyukii <49896401+dreamyukii@users.noreply.github.com>
2026-02-19 02:54:06 +0700

    fix: remove ptyxis stuff

--- build_files/base/16-override-install.sh
+++ build_files/base/16-override-install.sh
@@ -27,7 +27,7 @@ rm -rf /usr/share/doc/HTML
 PANEL_CONF="/usr/share/plasma/plasmoids/org.kde.plasma.taskmanager/contents/config/main.xml"
 
 if [[ -f "${PANEL_CONF}" ]]; then
-  sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:org.gnome.Ptyxis.desktop,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager<\/default>/' "${PANEL_CONF}"
+  sed -i '/<entry name="launchers" type="StringList">/,/<\/entry>/ s/<default>[^<]*<\/default>/<default>preferred:\/\/browser,applications:io.github.kolunmi.Bazaar.desktop,preferred:\/\/filemanager<\/default>/' "${PANEL_CONF}"
 fi
 
 # Hide Discover entries by renaming them (allows for easy re-enabling)
@@ -48,18 +48,8 @@ done
 rm /etc/xdg/autostart/org.kde.discover.notifier.desktop
 
 # Use Bazaar for Flatpak refs
-# Force Ptyxis version opened via dbus (e.g., keyboard shortcut) to use the proper shim
 # https://github.com/ublue-os/bazzite/pull/3620
-sed -i 's@Exec=/usr/bin/ptyxis@Exec=/usr/bin/kde-ptyxis@g' /usr/share/dbus-1/services/org.gnome.Ptyxis.service
 echo "application/vnd.flatpak.ref=io.github.kolunmi.Bazaar.desktop" >> /usr/share/applications/mimeapps.list
-
-# Ptyxis Terminal
-sed -i 's@\[Desktop Action new-window\]@\[Desktop Action new-window\]\nX-KDE-Shortcuts=Ctrl+Alt+T@g' /usr/share/applications/org.gnome.Ptyxis.desktop
-sed -i 's@Exec=ptyxis@Exec=kde-ptyxis@g' /usr/share/applications/org.gnome.Ptyxis.desktop
-sed -i 's@Keywords=@Keywords=konsole;console;@g' /usr/share/applications/org.gnome.Ptyxis.desktop
-# GTK 4.20 changed how it handles input methods; see https://github.com/ghostty-org/ghostty/discussions/8899#discussioncomment-14717979
-desktop-file-edit --set-key=Exec --set-value='env GTK_IM_MODULE=ibus kde-ptyxis' /usr/share/applications/org.gnome.Ptyxis.desktop
-cp /usr/share/applications/org.gnome.Ptyxis.desktop /usr/share/kglobalaccel/
 
 rm -f /etc/profile.d/gnome-ssh-askpass.{csh,sh} # This shouldn't be pulled in
 
