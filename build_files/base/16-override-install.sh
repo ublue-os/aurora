9473e96c5b8cba2f8209c5d574742fb9ee811486
renner <renner0@posteo.de>
2026-03-14 20:14:09 +0100

    fix: remove ptyxis stuff

--- build_files/base/16-override-install.sh
+++ build_files/base/16-override-install.sh
@@ -39,18 +39,8 @@ done
 rm /etc/xdg/autostart/org.kde.discover.notifier.desktop
 
 # Use Bazaar for Flatpak refs
-# Force Ptyxis version opened via dbus (e.g., keyboard shortcut) to use the proper shim
 # https://github.com/ublue-os/bazzite/pull/3620
-sed -i 's@Exec=/usr/bin/ptyxis@Exec=/usr/bin/kde-ptyxis@g' /usr/share/dbus-1/services/org.gnome.Ptyxis.service
 echo "application/vnd.flatpak.ref=io.github.kolunmi.Bazaar.desktop" >> /usr/share/applications/mimeapps.list
-
-# Ptyxis Terminal
-sed -i 's@\[Desktop Action new-window\]@\[Desktop Action new-window\]\nX-KDE-Shortcuts=Ctrl+Alt+T@g' /usr/share/applications/org.gnome.Ptyxis.desktop
-sed -i 's@Exec=ptyxis@Exec=kde-ptyxis@g' /usr/share/applications/org.gnome.Ptyxis.desktop
-sed -i 's@Keywords=@Keywords=konsole;console;@g' /usr/share/applications/org.gnome.Ptyxis.desktop
-# GTK 4.20 changed how it handles input methods; see https://github.com/ghostty-org/ghostty/discussions/8899#discussioncomment-14717979
-desktop-file-edit --set-key=Exec --set-value='env GTK_IM_MODULE=ibus kde-ptyxis' /usr/share/applications/org.gnome.Ptyxis.desktop
-cp /usr/share/applications/org.gnome.Ptyxis.desktop /usr/share/kglobalaccel/
 
 rm -f /etc/profile.d/gnome-ssh-askpass.{csh,sh} # This shouldn't be pulled in
 
