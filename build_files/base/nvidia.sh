13c7861cd7d54f47502d1bf4977d3e609db9f199
inffy <10782843+inffy@users.noreply.github.com>
2026-03-16 08:27:04 +0200

    chore: enable nvidia-container-toolkit

--- build_files/base/nvidia.sh
+++ build_files/base/nvidia.sh
@@ -4,9 +4,6 @@ echo "::group:: ===$(basename "$0")==="
 
 set -eoux pipefail
 
-# Not available for Fedora 43 yet
-dnf config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
-
 IMAGE_NAME="${BASE_IMAGE_NAME}" AKMODNV_PATH="/tmp/rpms/nvidia" MULTILIB=0 /tmp/rpms/nvidia/ublue-os/nvidia-install.sh
 rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
 ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
