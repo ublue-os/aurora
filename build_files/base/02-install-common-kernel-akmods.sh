1fec6b69da1d6b31ae643f81a0702b6a68523305
inffy <10782843+inffy@users.noreply.github.com>
2026-03-17 08:19:46 +0200

    chore: remove openrazer from image
    Removed installation of OpenRazer packages and repository configuration.

--- build_files/base/02-install-common-kernel-akmods.sh
+++ build_files/base/02-install-common-kernel-akmods.sh
@@ -24,16 +24,11 @@ fi
 
 dnf5 versionlock add kernel kernel-devel kernel-devel-matched kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
-dnf -y install /tmp/rpms/{common,kmods}/*xone*.rpm /tmp/rpms/{common,kmods}/*openrazer*.rpm || true
+dnf -y install /tmp/rpms/{common,kmods}/*xone*.rpm || true
 
 dnf -y install /tmp/rpms/{kmods,common}/*v4l2loopback*.rpm || true
 
 mkdir -p /etc/pki/akmods/certs
 ghcurl "https://github.com/ublue-os/akmods/raw/refs/heads/main/certs/public_key.der" --retry 3 -Lo /etc/pki/akmods/certs/akmods-ublue.der
-
-# OpenRazer from hardware:razer repo (not a COPR)
-dnf -y config-manager addrepo --from-repofile=https://openrazer.github.io/hardware:razer.repo
-dnf -y install openrazer-daemon || true
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/hardware:razer.repo
 
 echo "::endgroup::"
