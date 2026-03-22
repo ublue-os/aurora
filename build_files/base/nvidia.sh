7f1f0721700575e0026a2497d00a77b848a7375a
inffy <10782843+inffy@users.noreply.github.com>
2026-03-22 09:42:56 +0200

    temp fix for nvidia

--- build_files/base/nvidia.sh
+++ build_files/base/nvidia.sh
@@ -5,6 +5,13 @@ echo "::group:: ===$(basename "$0")==="
 set -eoux pipefail
 
 IMAGE_NAME="${BASE_IMAGE_NAME}" AKMODNV_PATH="/tmp/rpms/nvidia" MULTILIB=0 /tmp/rpms/nvidia/ublue-os/nvidia-install.sh
+if [ -f "/etc/yum.repos.d/nvidia-container-toolkit.repo" ]; then
+    sed -i \
+        -e 's/^gpgcheck=0/gpgcheck=1/' \
+        -e 's/^enabled=0.*/enabled=1/' \
+        -e 's|^sslcacert=.*|sslcacert=/etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem|' \
+        "/etc/yum.repos.d/nvidia-container-toolkit.repo"
+fi
 rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
 ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
 tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<EOF
