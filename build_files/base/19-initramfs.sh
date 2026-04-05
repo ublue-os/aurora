d95590e37ebc6c8ee2c66d0d4f66aef062c2c18e
renner <80410025+renner0e@users.noreply.github.com>
2026-04-05 15:45:08 +0200

    fix: move dracut configs to files instead of flags (#2003)
    While this doesn't make a difference to our image build itself, it will
    make the life easier for derived images as they will inherit all the
    modules from us that we set anyway. This undoes some of the changes made
    in 32a66fc7191. These are specific to the things we install here so it
    doesn't make sense to go into common.

--- build_files/base/19-initramfs.sh
+++ build_files/base/19-initramfs.sh
@@ -2,13 +2,12 @@
 
 echo "::group:: ===$(basename "$0")==="
 
-set -oue pipefail
+set -ouex pipefail
 
 KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
 
-# Ensure Initramfs is generated
 export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v --add "ostree fido2 tpm2-tss pkcs11 pcsc" -f "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
+/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
 
 echo "::endgroup::"
