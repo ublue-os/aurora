5c1ac08af63592d64ab26c7dfcbb08ec5e077f43
renner <80410025+renner0e@users.noreply.github.com>
2026-07-18 08:35:29 +0200

    chore: remove duplicate options from dracut (#2572)
    These are already included in our base image as configuration files,
    there is no need to specify them on the cli again.
    
    See: https://gitlab.com/fedora/ostree/ci-test/-/blob/5f1f192f007b2a7049d027b967b4ed58a0454143/initramfs.yaml

--- build_files/base/19-initramfs.sh
+++ build_files/base/19-initramfs.sh
@@ -7,7 +7,7 @@ set -ouex pipefail
 KERNEL_VERSION=$(rpm -q --queryformat="%{evr}.%{arch}" kernel-core)
 
 export DRACUT_NO_XATTR=1
-/usr/bin/dracut --no-hostonly --kver "${KERNEL_VERSION}" --reproducible -v -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
+/usr/bin/dracut --kver "${KERNEL_VERSION}" --reproducible -v -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
 chmod 0600 "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
 
 echo "::endgroup::"
