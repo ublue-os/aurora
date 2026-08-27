e93c41edd7b6a9b5e186f8090c8dcd1b0354bbe9
renner <80410025+renner0e@users.noreply.github.com>
2026-08-27 04:04:18 +0000

    fix: introduce yet another hack for plasma-welcome (#2780)
    Seems like [1] doesn't succesfully avoid the crash [2] anymore.
    
    [1] https://github.com/get-aurora-dev/common/commit/28529d0da015bc80e8920a2e3604b4d413f24362
    [2] https://github.com/ublue-os/aurora/issues/2624

--- build_scripts/base/16-override-install.sh
+++ build_scripts/base/16-override-install.sh
@@ -44,4 +44,7 @@ ln -s /usr/share/applications/dev.getaurora.offline-docs.desktop /usr/share/kglo
 # Meta+Enter rules
 desktop-file-edit --set-key=X-KDE-Shortcuts --set-value='Ctrl+Alt+T,Meta+Return' /usr/share/applications/org.kde.konsole.desktop
 
+# HACK for https://github.com/ublue-os/aurora/issues/2624 https://bugs.kde.org/show_bug.cgi?id=523540
+desktop-file-edit --set-key=Exec --set-value=plasma-welcome-hack /usr/share/applications/org.kde.plasma-welcome.desktop
+
 echo "::endgroup::"
