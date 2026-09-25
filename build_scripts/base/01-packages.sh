e99e4ff9858e622939d220e204d93f5b72a1d7ed
Juha Uotila <10782843+inffy@users.noreply.github.com>
2026-09-25 08:04:40 +0300

    chore: allow possible downgrade to negativo packages

--- build_scripts/base/01-packages.sh
+++ build_scripts/base/01-packages.sh
@@ -25,11 +25,7 @@ OVERRIDES=(
     "mesa-vulkan-drivers"
 )
 
-# Remove libheif-ffmpeg from Fedora to avoid conflicts
-# https://packages.fedoraproject.org/pkgs/libheif/libheif/fedora-45.html
-dnf5 remove -y libheif-ffmpeg || true
-
-dnf5 distro-sync --skip-unavailable -y --repo='fedora-multimedia' "${OVERRIDES[@]}"
+dnf5 distro-sync --skip-unavailable --allowerasing -y --repo='fedora-multimedia' --from-vendor='negativo17.org' "${OVERRIDES[@]}"
 dnf5 versionlock add "${OVERRIDES[@]}"
 
 FEDORA_PACKAGES=(
